2be7b9e381d16787cd51fc8a4b9aa060232b9f02
lynh146 <nguyenthuylinhl680@gmail.com>
2025-07-02 19:08:27 +0700

    đã ghi chú file này cần làm những gì

---
+++ logout.php
@@ -0,0 +1,7 @@
+<!-- Đăng xuất người dùng 
+    - Xóa session người dùng
+    - Chuyển hướng về trang đăng nhập
+    - Hiển thị thông báo đăng xuất thành công
+    - Nếu người dùng chưa đăng nhập thì chuyển hướng về trang đăng nhập
+
+  -->
