59e8d15633a003831a8e562d76eb6668b8fa5d56
lynh146 <nguyenthuylinhl680@gmail.com>
2025-07-03 13:52:13 +0700

    đổi lại tên file quán nổi bật

---
+++ restaurants.php
@@ -0,0 +1,19 @@
+<!-- trang quán nổi bật: diễn
+ Trang hiển thị tất cả các quán nổi bật.
+Nội dung đầy đủ nên có cho mỗi quán:
+
+Thông Tin	                Mô tả
+Tên quán	                Dạng clickable dẫn đến trang chi tiết
+Ảnh đại diện	            Có thể là ảnh nền hoặc thumbnail
+Địa chỉ	                    Hiển thị rõ ràng
+Giá cả / Price Level	    “Rẻ”, “Trung bình”, “Đắt”
+Giờ mở cửa (ngày hiện tại)	Ví dụ: “Hôm nay mở cửa: 7h - 21h”
+Đánh giá trung bình	        Số sao hoặc điểm trung bình
+Lượt đánh giá	            Bao nhiêu người đánh giá
+Danh mục	                Ví dụ: “Mì Quảng”, “Hàn Quốc”, “Ăn vặt”
+Nút yêu thích (♥)	        Cho phép thêm vào mục yêu thích
+
+Bộ lọc theo giá, loại món, quận, giờ mở cửa
+
+Sort theo đánh giá cao nhất, mới nhất, nhiều yêu thích
+-->
