a2dc0a37ad7c7c7d1a6e812f6a96f5c387889ff8
vothedien <theneid28@gmail.com>
2025-07-04 22:17:44 +0700

    Add files via upload
    Signed-off-by: vothedien <theneid28@gmail.com>

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- index.php -->
 <?php include("includes/header.php"); ?>
-
+<link rel="stylesheet" href="../BTL-main/assets/css/quannoibat.css">
 <div class="hero">
     <div class="search-box">
         <input type="text" placeholder="Bạn muốn ăn gì?">
@@ -10,7 +10,7 @@
             <option>Quận 3</option>
         </select>
         <select>
-            <option>Mức giá</option>
+            <option>Mức giá</option> 
             <option>Rẻ</option>
             <option>Trung bình</option>
             <option>Đắt</option>
@@ -27,6 +27,38 @@
 <!-- QUÁN NỔI BẬT -->
 <section>
     <h2>Quán ăn nổi bật</h2>
+    <?php
+$conn = mysqli_connect("localhost", "root", "", "food_review", 3308);
+mysqli_set_charset($conn, "utf8");
+
+$sql = "SELECT id, name, location, price_level, image_url, rating
+        FROM restaurants
+        ORDER BY count DESC
+        LIMIT 5";
+$result = mysqli_query($conn, $sql);
+?>
+
+<div class="box-noibat">
+  <div class="box-header">
+    <a href="restaurants.php" style="text-decoration: none; color: #e74c3c;">
+      🔥 Quán ăn nổi bật
+    </a>
+  </div>
+  <ul class="box-list">
+    <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+      <li class="item">
+        <a href="chitiet.php?id=<?php echo $row['id']; ?>">
+          <img src="<?php echo $row['image_url']; ?>" class="thumb" alt="Ảnh">
+        </a>
+        <div class="info">
+          <a href="chitiet.php?id=<?php echo $row['id']; ?>" class="title"><?php echo $row['name']; ?></a>
+          <p class="meta">📍 <?php echo $row['location']; ?> | 💰 <?php echo $row['price_level']; ?></p>
+          <p class="rating">⭐ <?php echo number_format($row['rating'], 1); ?></p>
+        </div>
+      </li>
+    <?php } ?>
+  </ul>
+</div>
     <!-- 
     Dùng các hàm để tự n truy vấn hiển thị lên k được nhập tay từng quán, yêu cầu 3-6 quán hiển thị danh sách
     Tên quán
